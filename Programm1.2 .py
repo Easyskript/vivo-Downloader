435dd7c27e1a98428fd82c6ad170f3c5e474b7d0
Amdusias <74834221+Easyskript@users.noreply.github.com>
2020-11-22 16:07:34 +0100

    Update Programm1.2 .py

--- Programm1.2 .py
+++ Programm1.2 .py
@@ -1,4 +1,5 @@
 
+#only for JupyterNotebook(anaconda3)
 #put the  chromedriver exe in the same folder !!!!!!!
 #see your chrome version: https://www.whatismybrowser.com/detect/what-version-of-chrome-do-i-have
 #Download your versions chromedriver here: https://chromedriver.chromium.org/downloads
