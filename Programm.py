2d4373dd79612f747bc1ecfcda6b56373553fdda
Amdusias <74834221+Easyskript@users.noreply.github.com>
2020-11-22 15:53:22 +0100

    Add files via upload

---
+++ Programm.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+#-vorher bitte python installiern und dann das programm starten
+#-Achtung, ihr dürft nur euren eigenen Content Downloads!!!
+#-Dieses Programm darf nicht für Urheberrechts Verletzungen genutzt werden!!
+#-------------------------
+#-you need python!
+#-Attention, you are only allowed to download your own content !!!
+#-This program may not be used for copyright infringement !!
+#---------------------------------------------------------------------------
+get_ipython().system('pip install selenium')
+import os
+import webbrowser
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#--------------------------------------------------------------------------
+urlbasic = "https://9xbuddy.org/process?url=https%3A%2F%2Fvivo.sx%2F"
+url = "https://vivo.sx/51a103acd8"
+speak = input("do you speak german(1) oder english(2)")
+true = True
+#-------------------------
+def load(url):
+    url = url.strip("https://vivo.sx/")
+    finaleseite = (urlbasic + url)
+    webbrowser.open(finaleseite)
+    PATH = "./chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    driver.get(finaleseite)
+    time.sleep(2)
+    link = driver.find_element_by_xpath('(//SPAN)[36]')
+    link.click()
+#--------------------------------------
+def lan():
+    if speak == "1":
+        print("\n, Eiso sprichst du Deutsch finde ich gut ")
+        url = input("hier kommt der Vivo Link rein:  ")
+        load(url)
+    elif speak == "2":
+        print("\n nice you speak english Nice ")
+        url = input("Enter the Vivo URL pls:  ")
+        load(url)
+    else:
+        time.sleep(1)
+        lan()
+#-------------------------------------
+
+if True == True:
+    lan()
+
+
+# In[10]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
